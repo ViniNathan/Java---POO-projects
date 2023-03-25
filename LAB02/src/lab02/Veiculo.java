01c1b590846e7042418b8b9444fe2f7f386c34a7
Vini-nathan <viniciosnathan@gmail.com>
2023-03-25 08:58:04 -0300

    Commit dos arquivos do LAB02

---
+++ LAB02/src/lab02/Veiculo.java
@@ -0,0 +1,46 @@
+package lab02;
+
+public class Veiculo {
+    private String placa;
+    private String marca;
+    private String modelo;
+
+    public Veiculo(String placa, String marca, String modelo) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    // Padrão de "print"
+    public String toString() {
+        return "Veiculo{" +
+                "Placa='" + placa + '\'' +
+                ", Marca='" + marca + '\'' +
+                ", Modelo='" + modelo + '\'' +
+                '}';
+    }
+}
